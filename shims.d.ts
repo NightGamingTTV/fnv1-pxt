c3fde60e7255bb06e7fcf628d4d714d9fa43e04d
NightGamingTTV <108269876+NightGamingTTV@users.noreply.github.com>
2025-10-23 00:09:41 -0600

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -1,3 +1,11 @@
+// Auto-generated. Do not edit.
 declare namespace sec {
-    function FNV1aHash(data: string): number;
+
+    /**
+     * FNV-1a hash implementation
+     */
+    //% shim=sec::FNV1aHash
+    function FNV1aHash(data: string): int32;
 }
+
+// Auto-generated. Do not edit. Really.
