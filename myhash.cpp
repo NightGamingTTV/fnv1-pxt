f5a99abf12f3598ee5e30508ae864011ee484839
NightGamingTTV <108269876+NightGamingTTV@users.noreply.github.com>
2025-10-23 00:39:15 -0600

    Update myhash.cpp

--- myhash.cpp
+++ myhash.cpp
@@ -1,22 +1,19 @@
 #include "pxt.h"
-
 using namespace pxt;
 
 namespace sec {
-
-//%
-int FNV1aHash(StringData *data) {
-    if (data == NULL) return 0;
-    
-    uint32_t hash = 2166136261U;
-    int len = data->len;
-    
-    for (int i = 0; i < len; i++) {
-        hash ^= (uint8_t)data->data[i];
-        hash *= 16777619U;
+    //%
+    int FNV1aHash(String s) {
+        if (s == NULL) return 0;
+        
+        uint32_t hash = 2166136261U;
+        int len = s->getLength();
+        
+        for (int i = 0; i < len; i++) {
+            hash ^= (uint8_t)s->getChar(i);
+            hash *= 16777619U;
+        }
+        
+        return (int)hash;
     }
-    
-    return (int)hash;
-}
-
 }
