edd93e1411ade1e8528b24f18dc35d2b04e920de
NightGamingTTV <108269876+NightGamingTTV@users.noreply.github.com>
2025-10-23 00:48:00 -0600

    Update main.ts, myhash.cpp

--- main.ts
+++ main.ts
@@ -1,11 +1,6 @@
 //% weight=100 color=#d10000 icon="\uf02d" block="Custom Hash"
 namespace sec {
-    /**
-     * Calculates a custom FNV-1a hash of a string.
-     * @param data The string to hash
-     */
     //% block="FNV-1a hash of %data"
-    //% data.defl="hello"
     //% shim=sec::FNV1aHash
     export function FNV1aHash(data: string): number {
         return 0;
--- myhash.cpp
+++ myhash.cpp
@@ -1,22 +1,10 @@
 #include "pxt.h"
-
 using namespace pxt;
 
 namespace sec {
-
-//%
-int FNV1aHash(String s) {
-    if (s == NULL) return 0;
-
-    uint32_t hash = 2166136261U;
-    int len = GET_STRING_LENGTH(s);
-
-    for (int i = 0; i < len; i++) {
-        hash ^= (uint8_t)GET_STRING_CHAR(s, i);
-        hash *= 16777619U;
+    //%
+    int FNV1aHash(String s) {
+        // Test with fixed return first
+        return 12345;
     }
-
-    return (int)hash;
-}
-
 }
