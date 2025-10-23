5b2b0479be8515078ccac52e430dc091a5f7b10e
NightGamingTTV <108269876+NightGamingTTV@users.noreply.github.com>
2025-10-23 01:14:06 -0600

    Update shims.d.ts

---
+++ shims.d.ts
@@ -0,0 +1,12 @@
+// Auto-generated. Do not edit.
+declare namespace custom {
+
+    /**
+     * Calculates a custom hash value for a given string (FBV1).
+     * @param data The string to hash.
+     */
+    //% shim=custom::FBV1_C
+    function FBV1_C(data: string): uint32;
+}
+
+// Auto-generated. Do not edit. Really.
